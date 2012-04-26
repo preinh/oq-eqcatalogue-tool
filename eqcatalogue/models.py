a9bd9110a8400a3285b5a3a94ff2dbfd6c0b1952
Luigi Panzeri <defmacro@gmail.com>
2012-04-26 17:32:56 +0200

    catalogue schema. first pass

--- eqcatalogue/models.py
+++ eqcatalogue/models.py
@@ -17,7 +17,7 @@ def connect(dbapi_connection, connection_rec):
     dbapi_connection.enable_load_extension(True)
 
 
-"""Create the spatialite database and load the schema"""
+# Create the spatialite database and load the schema
 engine = sqlalchemy.create_engine('sqlite:///%s' % FILENAME, module=sqlite)
 metadata = sqlalchemy.MetaData(engine)
 Base = declarative_base(metadata=metadata)
@@ -35,7 +35,14 @@ except sqlalchemy.exc.OperationalError:
             raise RuntimeError("Could not load libspatial extension. Check your spatialite and pysqlite2 installation")
 
 class BaseMixin(object):
-    """A base class to inherit from for catalogue models"""
+    """A base class mixin to include for all the models. It provides the definition of the identifier and a timestamp.
+
+    :py:attribute:: id
+      Internal identifier
+
+    :py:attribute:: created_at
+      When this object has been imported into the catalogue db
+      """
 
     @declared_attr
     def __tablename__(cls):
@@ -44,41 +51,56 @@ class BaseMixin(object):
     __table_args__ = {}
     __mapper_args__= {}
 
-    "Internal identifier"
     id =  sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-
-    "When this objects has been imported into the catalogue db"
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now())
 
 class CatalogueBaseMixin(BaseMixin):
-    """A base class for models that can have an identifier (which can
-    be different from the internal one) local for each source"""
+    """A mixin for models that can have a local identifier (which can
+    be different from the internal one) for each event source (e.g.
+    solutionKey, eventKey)
 
-    "the identifier used by the event source for the object"
+    :py:attribute:: source_key
+    the identifier used by the event source for the object
+    """
     source_key = sqlalchemy.Column(sqlalchemy.String(), nullable=False)
 
-class EventSource(BaseMixin, Base):
-    """A source of event catalogues. E.g. ISC Web Catalogue"""
+    @declared_attr
+    def eventsource_id(cls):
+        return sqlalchemy.Column(sqlalchemy.Integer,
+                                 sqlalchemy.ForeignKey('catalogue_eventsource.id'))
 
-    "event short description."
+class EventSource(BaseMixin, Base):
+    """A source of event catalogues. E.g. ISC Web Catalogue
+
+    We assume that for each event source there is only one file format
+we import data from
+
+    :py:attribute:: name
+    an unique event source short name.
+    """
+
     name = sqlalchemy.Column(sqlalchemy.String(255), unique=True)
 
     def __repr__(self):
-        return "EventSource: %s" % self.name
+        return "Event Source: %s" % self.name
 
 class Agency(BaseMixin, Base):
-    """The agency which recorded and measured the events"""
+    """The agency which recorded and measured the events.
 
-    # agency short name. e.g. ISC, IDC, DMN
+    :py:attribute:: short_name
+    agency short name. e.g. ISC, IDC, DMN
+
+    :py:attribute:: name
+    agency long name
+
+    :py:attribute:: eventsource
+    the source object we have imported the agency from. It is unique
+    together with `short_name`
+"""
     short_name = sqlalchemy.Column(sqlalchemy.String(255))
-
-    # agency long name.
     name = sqlalchemy.Column(sqlalchemy.Unicode, nullable=True)
 
-    eventsource_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('catalogue_eventsource.id'))
     eventsource = orm.relationship("Source", backref=orm.backref('agencies', order_by='created_at'))
-
-    # unique together (eventsource_id and short_name)
 
     def __repr__(self):
         if self.name:
@@ -87,17 +109,42 @@ class Agency(BaseMixin, Base):
             return "Agency %s" % self.short_name
 
 class Event(CatalogueBaseMixin, Base):
-    """Describes an event"""
+    """Describes a sismic event.
 
-    eventsource_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('catalogue_eventsource.id'))
+    :py:attribute:: eventsource
+    the source object we have imported the agency from. unique
+    together with `source_key`
+"""
+
     eventsource = orm.relationship("Source", backref=orm.backref('events', order_by='created_at'))
-    # unique togethr (source_key and eventsource_id)
 
     def __repr__(self):
         return "Event %s (by %s)" % (self.source_key,
                                      self.source)
 
 class MagnitudeMeasure(BaseMixin, Base):
+    """Describes a single measure of the magnitude of an event
+
+    :py:attribute:: event
+    the event object associated with this measure
+
+    :py:attribute:: agency
+    the agency that has provided the measure
+
+    :py:attribute:: origin
+    the origin related to this measure
+
+    :py:attribute:: scale
+    the scale used for this measure. It is unique together with `agency_id` and `origin_id`
+
+    :py:attribute:: value
+    the magnitude expressed in the unit suitable for the scale used
+
+    :py:attribute:: error
+    the magnitude error (FIXME: please tell me if it is relative or absolute)
+
+    """
+
     event_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('catalogue_event.id'))
     event = orm.relationship("Event", backref=orm.backref('measures', order_by=id))
 
@@ -112,33 +159,81 @@ class MagnitudeMeasure(BaseMixin, Base):
 
     absolute_error = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
 
-    # unique together (scale, agency_id and origin_id)
-
     def __repr__(self):
-        return "measure of event %s (origin: %s, time: %s"
+        return "measure of %s at %s by %s: %s %s" % (self.event, self.origin, self.agency, self.scale, self.value)
 
 class Origin(BaseMixin, Base):
+    """Describes a point at a given depth and a time. For each quantity a measure of the  accuracy is described.
+
+    :py:attribute:: time
+    Time in format "YYYY-MM-DD HH:MM:SS.SSS".
+
+    :py:attribute:: time_error
+    Time errors expressed in seconds.
+
+    :py:attribute:: time_rms
+    Time error expressed as a Root Mean Square in seconds.
+
+    :py:attribute:: position
+    Point coordinate (latitude and longitude)
+
+    :py:attribute:: semi_major_90error
+    Semi-Major axis of the 90th percentile confidence ellipsis of the
+    epicentre.
+
+    :py:attribute:: semi_minor_90error
+    Semi-Minor axis of the 90th percentile confidence ellipsis of the
+    epicentre.
+
+    :py:attribute:: azimuth_error
+    Azimuth with respect to geographical north of the Semi-Major axis.
+
+    :py:attribute:: depth
+    depth of the hypocentre in km.
+
+    :py:attribute:: depth_error
+    Error in km on the hypocentre depth.
+"""
+
     time = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
 
     time_error = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
     time_rms = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
 
     position = geoalchemy.GeometryColumn(geoalchemy.Point(2))
+
     semi_minor_90error = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
     semi_major_90error = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
-    azimuth_error = sqlalchemy.Column(sqlalchemy.Float(), nullable=True)
 
     depth = sqlalchemy.Column(sqlalchemy.Float(), nullable=False)
     depth_error = sqlalchemy.Column(sqlalchemy.Float(), nullable=False)
 
 class MeasureMetadata(BaseMixin, Base):
+    """Metadata of a measurement.
+
+    :py:attribute:: magnitudemeasure
+    the measure, the metadata is associated with
+
+    :py:attribute:: name
+    the name of the metadata. It is unique together with magnitudemeasure
+    """
+
+    METADATA_TYPES = ('phases', 'stations',
+                      'azimuth_gap', 'azimuth_error',
+                      'min_distance', 'max_distance',
+                      'num_stations')
+
     magnitudemeasure_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('catalogue_magnitudemeasure.id'))
     magnitudemeasure = orm.relationship("MagnitudeMeasure", backref=orm.backref('metadatas', order_by='name'))
 
-    name = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    name = sqlalchemy.Enum(METADATA_TYPES, nullable=False)
     value = sqlalchemy.Column(sqlalchemy.Float(), nullable=False)
 
-    # unique_together(measure_id, name)
 
+def create_all():
+    Base.metadata.create_all(engine)
+    geoalchemy.GeometryDDL(Origin.__table__)
 
-Base.metadata.create_all(engine)
+def recreate_all():
+    Base.metadata.drop_all() # can fail it tables are not present
+    create_all()
