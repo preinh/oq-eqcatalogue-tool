b2850c77e9e403f4790d1963e76c32a81af41ebe
Luigi Panzeri <defmacro@gmail.com>
2012-06-09 01:03:48 +0200

    fixed qa tests

--- qa_tests/test_acceptance.py
+++ qa_tests/test_acceptance.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from tests.test_utils import in_data_dir
 
 from eqcatalogue import models, selection
-from eqcatalogue.managers import EventManager
+from eqcatalogue.filters import EventFilter
 from eqcatalogue.regression import (EmpiricalMagnitudeScalingRelationship,
                                     LinearModel, PolynomialModel)
 from eqcatalogue.serializers import mpl
@@ -19,7 +19,7 @@ class CatalogueTool(unittest.TestCase):
 
     def test_first_config(self):
         # Apply event selection
-        events = EventManager().with_agencies("ISC", "BJI")
+        events = EventFilter().with_agencies("ISC", "BJI")
 
         # Computing Magnitude Scaling Relationship
         emsr = EmpiricalMagnitudeScalingRelationship.make_from_events(
@@ -35,7 +35,7 @@ class CatalogueTool(unittest.TestCase):
 
     def test_second_config(self):
         # Apply event selection
-        events = EventManager().with_agencies("ISC").before(datetime.now())
+        events = EventFilter().with_agencies("ISC").before(datetime.now())
 
         # Computing Magnitude Scaling Relationship
         ranking = {"ML": ["ISC", "IDC"], "mb": ["ISC", "FUNV"]}
@@ -54,7 +54,7 @@ class CatalogueTool(unittest.TestCase):
     def test_third_config(self):
         # Apply event Selection
         polygon = 'POLYGON((127.40 30.24, 144.36 49.96, 150.22 34.78))'
-        events = EventManager().\
+        events = EventFilter().\
             between(datetime(2010, 2, 28, 4, 11), datetime.now()).\
             within_polygon(polygon).\
             with_agencies("NIED", "IDC").\
@@ -75,7 +75,7 @@ class CatalogueTool(unittest.TestCase):
 
     def test_fourth_config(self):
         # Apply event Selection
-        events = EventManager().\
+        events = EventFilter().\
             between(datetime(2010, 2, 28, 4, 11), datetime.now()).\
             with_agencies("NEIC", "IDC").\
             with_magnitudes("ML", "mb")
@@ -98,7 +98,7 @@ class CatalogueTool(unittest.TestCase):
         # Apply event Selection
         point = 'POINT(138.80 33.80)'
         distance = 10000000
-        events = EventManager().\
+        events = EventFilter().\
             within_distance_from_point(point, distance)
 
         # Computing Magnitude Scaling Relationship
