23674314db7303e40cc9d8b780502ec37bb0a679
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2012-06-11 15:32:51 +0200

    updated test_acceptance, removed old cruft

--- qa_tests/test_acceptance.py
+++ qa_tests/test_acceptance.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from tests.test_utils import in_data_dir
 
 from eqcatalogue import models, selection
-from eqcatalogue.filtering import EventFilter
+from eqcatalogue.filtering import MeasureFilter
 from eqcatalogue.regression import (EmpiricalMagnitudeScalingRelationship,
                                     LinearModel, PolynomialModel)
 from eqcatalogue.serializers import mpl
@@ -60,102 +60,3 @@ class HomogeniserAPI(unittest.TestCase):
         homo.set_missing_uncertainty_strategy(selection.MUSDiscard)
         self._plot_and_assert(homo, 'fifth')
 
-
-class CatalogueTool(unittest.TestCase):
-
-    def test_first_config(self):
-        # Apply event selection
-        events = EventFilter().with_agencies("ISC", "BJI")
-
-        # Computing Magnitude Scaling Relationship
-        emsr = EmpiricalMagnitudeScalingRelationship.make_from_events(
-            "mb", "MS", events, selection.PrecisionStrategy)
-        emsr.apply_regression_model(LinearModel)
-
-        # Plotting results
-        graph_filename = 'first_config.png'
-        mpl.plot(emsr, in_data_dir(graph_filename))
-
-        # Assert
-        self.assertTrue(os.path.exists(in_data_dir(graph_filename)))
-
-    def test_second_config(self):
-        # Apply event selection
-        events = EventFilter().before(datetime.now())
-
-        # Computing Magnitude Scaling Relationship
-        ranking = {"ML": ["ISC", "IDC"], "mb": ["ISC", "FUNV"]}
-        emsr = EmpiricalMagnitudeScalingRelationship.make_from_events(
-            "ML", "mb", events, selection.AgencyRankingStrategy(ranking))
-        emsr.apply_regression_model(LinearModel)
-        emsr.apply_regression_model(PolynomialModel, order=2)
-
-        # Plotting results
-        graph_filename = 'second_config.png'
-        mpl.plot(emsr, in_data_dir(graph_filename))
-
-        # Assert
-        self.assertTrue(os.path.exists(in_data_dir(graph_filename)))
-
-    def test_third_config(self):
-        # Apply event Selection
-        polygon = 'POLYGON((127.40 30.24, 144.36 49.96, 150.22 34.78))'
-        events = EventFilter().\
-            between(datetime(2010, 2, 28, 4, 11), datetime.now()).\
-            within_polygon(polygon).\
-            with_agencies("NIED", "IDC").\
-            with_magnitude_scales("ML", "mb")
-
-        # Computing Magnitude Scaling Relationship
-        emsr = EmpiricalMagnitudeScalingRelationship.make_from_events("ML",
-            "mb", events, selection.PrecisionStrategy,
-            missing_uncertainty_strategy=selection.MUSSetEventMaximum())
-        emsr.apply_regression_model(LinearModel)
-
-        # Plotting results
-        graph_filename = 'third_config.png'
-        mpl.plot(emsr, in_data_dir(graph_filename))
-
-        # Assert
-        self.assertTrue(os.path.exists(in_data_dir(graph_filename)))
-
-    def test_fourth_config(self):
-        # Apply event Selection
-        events = EventFilter().\
-            between(datetime(2010, 2, 28, 4, 11), datetime.now()).\
-            with_agencies("NEIC", "IDC").\
-            with_magnitude_scales("ML", "mb")
-
-        # Computing Magnitude Scaling Relationship
-        emsr = EmpiricalMagnitudeScalingRelationship.make_from_events("ML",
-            "mb", events, selection.PrecisionStrategy,
-            missing_uncertainty_strategy=selection.MUSSetDefault(0.2))
-        emsr.apply_regression_model(LinearModel)
-        emsr.apply_regression_model(PolynomialModel, order=3)
-
-        # Plotting results
-        graph_filename = 'fourth_config.png'
-        mpl.plot(emsr, in_data_dir(graph_filename))
-
-        # Assert
-        self.assertTrue(os.path.exists(in_data_dir(graph_filename)))
-
-    def test_fifth_config(self):
-        # Apply event Selection
-        point = 'POINT(138.80 33.80)'
-        distance = 10000000
-        events = EventFilter().\
-            within_distance_from_point(point, distance)
-
-        # Computing Magnitude Scaling Relationship
-        emsr = EmpiricalMagnitudeScalingRelationship.make_from_events("ML",
-            "mb", events, selection.PrecisionStrategy,
-            missing_uncertainty_strategy=selection.MUSDiscard())
-        emsr.apply_regression_model(LinearModel)
-
-        # Plotting results
-        graph_filename = 'fifth_config.png'
-        mpl.plot(emsr, in_data_dir(graph_filename))
-
-        # Assert
-        self.assertTrue(os.path.exists(in_data_dir(graph_filename)))
