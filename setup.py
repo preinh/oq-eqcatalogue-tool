43ba4b59e0cc253aa88809abd1bb8a93dd0622e7
Luigi Panzeri <defmacro@gmail.com>
2012-08-30 18:24:46 +0200

    new installation procedure. first pass

--- setup.py
+++ setup.py
@@ -23,33 +23,6 @@ from setuptools import setup, find_packages
 version = "0.01"
 url = "http://github.com/gem/oq-eqcatalogue-tool"
 
-import re
-
-
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
-
-    return requirements
-
-
-def parse_dependency_links(file_name):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
-
-    return dependency_links
-
-
 setup(
     name='eqcatalogue-tool',
     version=version,
@@ -57,8 +30,6 @@ setup(
     long_description=__doc__,
     url=url,
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=parse_requirements('requirements.txt'),
-    dependency_links=parse_dependency_links('requirements.txt'),
     maintainer='GEM',
     maintainer_email='info@openquake.org',
     classifiers=(
