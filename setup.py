da4979df6cf0b38ecb11debca6974392349c78a8
Luigi Panzeri <defmacro@gmail.com>
2012-08-30 18:19:15 +0200

    new installation procedure. first pass

---
+++ setup.py
@@ -0,0 +1,76 @@
+# eqcatalogue-tool: Earthquake Catalogue Tool
+# Copyright (C) 2012 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Earthquake Catalogue Tool
+Copyright (C) 2012 GEM Foundation.
+"""
+from setuptools import setup, find_packages
+
+
+version = "0.01"
+url = "http://github.com/gem/oq-eqcatalogue-tool"
+
+import re
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
+
+
+setup(
+    name='eqcatalogue-tool',
+    version=version,
+    description="eqcatalogue performs earthquake catalogue homogenization",
+    long_description=__doc__,
+    url=url,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
+    maintainer='GEM',
+    maintainer_email='info@openquake.org',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Scientific/Engineering',
+    ),
+    keywords="earthquake catalog",
+    license="GNU AGPL v3",
+    platforms=["any"]
+)
