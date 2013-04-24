d47c66a8f9f554b5563481ed2459b160b3d76b23
Michele Simionato <michele.simionato@gmail.com>
2013-04-24 16:14:48 +0200

    Implemented download exposure from OQ Platform

--- openquake/qgis/gemcatalogue/catalogue.py
+++ openquake/qgis/gemcatalogue/catalogue.py
@@ -27,6 +27,8 @@
 """
 
 import uuid
+import httplib
+import tempfile
 # Import the PyQt and QGIS libraries
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -43,6 +45,8 @@ import os
 
 FMT_MAP = {ImporterDialog.ISF_PATTERN: V1,
            ImporterDialog.IASPEI_PATTERN: Iaspei}
+
+OQ_PLATFORM = "oq-platform-mn.gem.lan"
 
 
 def to_year(value):
@@ -91,18 +95,23 @@ class EqCatalogue:
             QIcon(":/plugins/eqcatalogue/icon.png"),
             u"Import catalogue file in db", self.iface.mainWindow())
 
+        self.get_exposure_action = QAction(
+            QIcon(":/plugins/eqcatalogue/icon.png"),
+            u"Get the exposure from the GEM platform", self.iface.mainWindow())
+
         # connect the action to the run method
         QObject.connect(self.dock, SIGNAL("visibilityChanged(bool)"),
                         self.update_toggle_status)
         QObject.connect(self.show_catalogue_action, SIGNAL("triggered()"),
                         self.toggle_dock)
-
         self.import_action.triggered.connect(self.show_import_dialog)
+        self.get_exposure_action.triggered.connect(self.show_exposure)
 
         # Add toolbar button and menu item
         self.iface.addToolBarIcon(self.show_catalogue_action)
         self.iface.addPluginToMenu(u"&eqcatalogue", self.show_catalogue_action)
         self.iface.addPluginToMenu(u"&eqcatalogue", self.import_action)
+        self.iface.addPluginToMenu(u"&eqcatalogue", self.get_exposure_action)
 
         self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
 
@@ -112,6 +121,7 @@ class EqCatalogue:
         self.iface.removePluginMenu(
             u"&eqcatalogue", self.show_catalogue_action)
         self.iface.removePluginMenu(u"&eqcatalogue", self.import_action)
+        self.iface.removePluginMenu(u"&eqcatalogue", self.get_exposure_action)
 
     def toggle_dock(self):
         # show the dock
@@ -143,6 +153,43 @@ class EqCatalogue:
             self.create_db(self.import_dialog.import_file_path,
                            str(self.import_dialog.fmt),
                            self.import_dialog.save_file_path)
+
+    def show_exposure(self):
+
+        crsSrc = self.iface.mapCanvas().mapRenderer().destinationCrs()
+        crsDest = QgsCoordinateReferenceSystem(4326)  # WGS 84 / UTM zone 33N
+        xform = QgsCoordinateTransform(crsSrc, crsDest)
+
+        extent = self.iface.mapCanvas().extent()
+        extent = xform.transform(extent)
+        lon_min, lon_max = extent.xMinimum(), extent.xMaximum()
+        lat_min, lat_max = extent.yMinimum(), extent.yMaximum()
+
+        # download data
+        c = httplib.HTTPSConnection(OQ_PLATFORM)
+        c.request("GET", '/exposure/population.json?lat1=%s&lng1=%s&'
+                  'lat2=%s&lng2=%s&output_type=csv' %
+                  (lat_min, lon_min, lat_max, lon_max))
+        response = c.getresponse()
+        assert response.status == 200, response.status
+
+        # save csv on a temporary file
+        fd, fname = tempfile.mkstemp(suffix='.csv')
+        os.close(fd)
+        # TODO: the server should give the size of the data
+        with open(fname, 'w') as csv:
+            while True:
+                data = response.read(10000)
+                if not data:
+                    break
+                csv.write(data)
+        uri = 'file://%s?delimiter=%s&xField=%s&yField=%s&crs=epsg:4326&' \
+            'skipLines=25&trimFields=yes' % (fname, ',', 'lat', 'lon')
+        try:
+            vlayer = QgsVectorLayer(uri, 'exposure_export', 'delimitedtext')
+            QgsMapLayerRegistry.instance().addMapLayer(vlayer)
+        finally:
+            return  # os.remove(fname)
 
     def update_map(self, agencies_selected, mscales_selected, mag_range,
                    date_range):
