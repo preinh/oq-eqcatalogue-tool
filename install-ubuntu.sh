fb069cf2e421ea61f1d8c94da7bc7833a5024412
Michele Simionato <michele@michele-VirtualBox.(none)>
2013-06-22 06:13:26 +0200

    Fixed the install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+set -x
 # sudo apt-get install git
 # git clone https://github.com/gem/oq-eqcatalogue-tool.git
 # bash install-ubuntu.sh precise|quantal|raring
 sudo apt-get install python-pip python-scipy python-shapely
 sudo apt-get install python-mock python-coverage python-nose
 sudo apt-get install python-pysqlite2 python-sqlalchemy
-sudo apt-get install python-twill matplotlib
+sudo apt-get install python-twill python-matplotlib
 sudo pip install geoalchemy
 
-sudo echo "\ndeb     http://qgis.org/debian-nightly $1 main\ndeb-src http://qgis.org/debian-nightly $1 main\n" > /etc/apt/sources.list
+sudo sed -i "$ a\deb     http://qgis.org/debian-nightly $1 main\ndeb-src http://qgis.org/debian-nightly $1 main\n" /etc/apt/sources.list
 sudo gpg --keyserver keyserver.ubuntu.com --recv 997D3880
 sudo gpg --export --armor 997D3880 | sudo apt-key add -
-sudo apt-get install qgis
+sudo apt-get update
+sudo apt-get install -y qgis
 mkdir $HOME/.qgis2
 mkdir $HOME/.qgis2/python
 mkdir $HOME/.qgis2/python/plugins
